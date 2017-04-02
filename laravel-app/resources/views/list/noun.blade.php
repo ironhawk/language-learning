6b02ae39417ceb5181564a1bc8e3b34c6359df42
ironhawk <attilaw@wwwind.hu>
2017-04-02 21:15:19 +0200

    névelő a főneveknél a listába

--- laravel-app/resources/views/list/noun.blade.php
+++ laravel-app/resources/views/list/noun.blade.php
@@ -1,5 +1,5 @@
 <td>
-	<a href="/view/noun/{{ $word->id }}/foreign">{{ $word->foreign }}</a>
+	<a href="/view/noun/{{ $word->id }}/foreign">{{ $word->definite_article }} {{ $word->foreign }} ({{ $word->plural }})</a>
 </td>
 <td>
 	<a href="/view/noun/{{ $word->id }}/hu">{{ $word->hu }}</a>
